1f8e9287f65b2aa11adf53bdcdd2d0b317ddb3a0
DSV08 <danilo.santos.vieira.contato@gmail.com>
2022-04-19 20:06:01 -0300

    Limpando comentarios

--- Lab1/ativo.py
+++ Lab1/ativo.py
@@ -12,16 +12,6 @@ sock = socket.socket()
 # conecta-se com o par passivo
 sock.connect((HOST, PORTA))
 
-# # envia uma mensagem a ser ecoada
-# msg = input()
-# sock.send(msg.encode('utf-8'))
-
-# #espera a resposta do par conectado (chamada pode ser BLOQUEANTE)
-# echo = sock.recv(1024)
-
-# # imprime a mensagem recebida
-# print(str(echo,  encoding='utf-8'))
-
 while True:
     #Capturando a mensagem e verificando se corresponde a mensagem para terminar o eco que e
     #encerrar eco
